c16b9bdaa8c71e0541b99a6c9027ec9eb9014356
eduh <123892448+adsfy@users.noreply.github.com>
2024-01-19 15:42:33 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1705689232,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sp_fundo.png",
+		"images/sp_obstaculo-sheet0.png",
+		"images/sp_chao-sheet0.png",
+		"images/sp_obstaculo2-sheet0.png",
+		"images/sp_player-sheet0.png",
+		"images/sp_player-sheet1.png",
+		"images/sp_player-sheet2.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
